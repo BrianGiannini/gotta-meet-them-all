53e2ec4885c708cf1c9b9ceafd254131e1812a58
Florian GUYET <florian.guyet@outlook.com>
2022-01-22 15:44:05 +0100

    Link adapter to fragment

--- app/src/main/java/com/devathons/gottameetthemall/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/devathons/gottameetthemall/dashboard/DashboardFragment.kt
@@ -1,7 +1,25 @@
 package com.devathons.gottameetthemall.dashboard
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.devathons.gottameetthemall.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.devathons.gottameetthemall.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+class DashboardFragment : Fragment() {
+    private val viewBinding by lazy { FragmentDashboardBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val persons = listOf(1, 2, 3, 4, 5)
+        val personsAdapter = PersonsAdapter(persons)
+        with(viewBinding.personsRecyclerView) {
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+            adapter = personsAdapter
+        }
+
+        return viewBinding.root
+    }
 }
--- app/src/main/java/com/devathons/gottameetthemall/dashboard/PersonsAdapter.kt
+++ app/src/main/java/com/devathons/gottameetthemall/dashboard/PersonsAdapter.kt
@@ -1,36 +1,23 @@
 package com.devathons.gottameetthemall.dashboard
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.devathons.gottameetthemall.R
+import com.devathons.gottameetthemall.databinding.PersonLayoutBinding
 
 class PersonsAdapter(private val persons: List<Int>) : RecyclerView.Adapter<PersonsAdapter.ViewHolder>() {
-
-    // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.person_layout, parent, false)
-
+        val view = PersonLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val person = persons[position]
-        // adapt view according to person data
+        holder.viewBinding.personNameTextView.text = person.toString()
     }
 
-    override fun getItemCount(): Int {
-        return persons.size
-    }
+    override fun getItemCount(): Int = persons.size
 
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.person_image_view)
-        val textView: TextView = itemView.findViewById(R.id.person_name_text_view)
-    }
+    class ViewHolder(val viewBinding: PersonLayoutBinding) : RecyclerView.ViewHolder(viewBinding.root)
 }
+
