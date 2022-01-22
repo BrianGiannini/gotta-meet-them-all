2428c160bdf00bcb0d0f5c436d056151a78c4511
Brian Giannini <brian.giannini.1@gmail.com>
2022-01-22 11:38:38 +0100

    add Timber

---
+++ app/src/main/java/com/devathons/gottameetthemall/MyApplication.kt
@@ -0,0 +1,17 @@
+package com.devathons.gottameetthemall
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+    }
+
+}
